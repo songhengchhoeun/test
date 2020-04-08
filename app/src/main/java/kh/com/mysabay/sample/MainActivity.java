f633e7e6b98f18fd0eb6278921308f1f045ae8f2
Tan Phirum <phirumtan@gmail.com>
2020-04-08 21:48:50 +0700

    update readme

--- app/src/main/java/kh/com/mysabay/sample/MainActivity.java
+++ app/src/main/java/kh/com/mysabay/sample/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         final SdkConfiguration configuration = new SdkConfiguration.Builder(
-                BuildConfig.APPLICATION_ID, // mysabay app Id
+                "55", // mysabay app Id
                 "SDK sample", //mysabay  app name
                 "9c85c50a4362f687cd4507771ba81db5cf50eaa0b3008f4f943f77ba3ac6386b", //MySabay App Secret
                 "", // license key
